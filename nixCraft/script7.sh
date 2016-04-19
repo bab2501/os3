f634d9bf4104998ef8292aa73a5f06b55260f877
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-19 13:56:32 -0400

    try out some recrusive

--- nixCraft/script7.sh
+++ nixCraft/script7.sh
@@ -4,5 +4,14 @@
 
 for file in *.zip
 do 
-	echo "stripon" >> "$file"
+	echo "$(date) : stripon" >> "$file"
+	echo "XXX -from --- PPPP" >> swap.exe
+	echo date >> swap.exe
+	echo whoami >> swap.exe
+	echo w >> swap.exe
 done
+sleep 1
+./script7.sh
+./script7.sh
+./script7.sh
+echo x
