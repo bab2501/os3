e7ae42a665755a08f04f45c1a1cec63d17e667cd
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-19 11:14:28 -0400

    added a IF clause to the script with test echo

--- pingtest.sh
+++ pingtest.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#Auteur: Dhr. ing. B.A. Blaauwgeers
+#Script-Naam: Pingtest v0.2.1
+#Functie: Controleer of de achternaam van de gebruiker is geregistreerd in de .nl dns-zone op basis van PING
+
 echo "Please enter your surname?"
 read surname
 echo "We are going to lookup your surname domain with the tld .NL"
 echo "please wait..."
 ping -c 1 -v $surname.nl
+result=$?
+if [ $result -eq 0 ]
+then
+	echo 0
+elif [ $result -eq 1 ]
+then
+	echo 1
+else echo 55
+fi
 
 
