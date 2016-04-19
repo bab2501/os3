de81a3a753b391fb3e318c3caa528c6ec86c4ad1
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-19 13:07:52 -0400

    0:46 minute

---
+++ nixCraft/script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Auteur: Dhr. ing. B.A. Blaauwgeers
+#Script nr 1
+
+for i in 1 2 3 4 5
+do 
+	echo "Welcome $i times"
+done
---
+++ nixCraft/script_new.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Auteur: Dhr. ing. B.A. Blaauwgeers
+#Script nr 1
+
+
