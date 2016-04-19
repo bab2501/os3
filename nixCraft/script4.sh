48d0b68f6c16cd9224492f385842a889f55bbf44
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-19 13:19:11 -0400

    2:14 C style for loop in bash

---
+++ nixCraft/script4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Auteur: Dhr. ing. B.A. Blaauwgeers
+#Script nr 4: three-expression syntax
+
+#share a common heritage with the C programming language
+
+for (( c=1; c<=5; c++ ))
+do 
+	echo "Welcome $c times"
+done
