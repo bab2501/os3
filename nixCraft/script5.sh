557ac666b0106c23d5d04ca28f511143a0deca67
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-19 13:21:38 -0400

    2:40 now with letters

---
+++ nixCraft/script5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Auteur: Dhr. ing. B.A. Blaauwgeers
+#Script nr 5: bash for loop with lists and letters
+
+#share a common heritage with the C programming language
+
+for a in {a..e}
+do 
+	echo "Letter : $a"
+done
