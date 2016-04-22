c9fca3f2199a88e642002e23f182915f3ca68fa2
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-22 07:27:40 -0400

    Wrote IF clauses

--- forloopif.sh
+++ forloopif.sh
@@ -3,3 +3,10 @@
 #Datum: 22.04.2016
 #Scriptname: forloopif v1
 #Func: Script without a function
+
+if [ $UID > 0]; then; echo "01: Always true"; fi
+if [[ $UID > 0 ]]; then; echo "02: You are not root"; fi
+if [ $UID -eq 0 ]; then; echo "03: You are root"; fi
+if [ $(id -u) > 0 ]; then; echo "04: Always true"; fi
+if [ $(id -u) -eq ]; then; echo "05: You are root"; fi
+if [ $(id -u) -gt ]; then; echo "06: You aren't root"; fi
