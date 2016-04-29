80d9dbc229f5ce0ac660cd4ef82221e31dad5dfa
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 05:06:23 -0400

    pagina 1

---
+++ linuxconfig-org/hello_world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author: Dhr. ing. B.A. Blaauwgeers
+# C: linuxconfig.org
+# T: Hallo World Bash Sell Script
+
+# declare  STRING variable
+STRING="Hello World"
+#print variable on screen
+echo $STRING
