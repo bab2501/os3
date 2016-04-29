260b94db29dd06aaf58fdf447a0303fbb725ae30
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 08:52:34 -0400

    6) with sleeptimer 10

--- linuxconfig-org/bash_trap.sh
+++ linuxconfig-org/bash_trap.sh
@@ -16,11 +16,31 @@ clear;
 bashtrap()
 {
 	echo "CTRL+C Detected !... executing bash trap!"
+	echo "Nog 10 seconden slapen"
+        sleep 1
+        echo "Nog 9 seconden slapen"
+        sleep 1
+        echo "Nog 8 seconden slapen"
+        sleep 1
+        echo "Nog 7 seconden slapen"
+        sleep 1
+        echo "Nog 6 seconden slapen"
+        sleep 1
+        echo "Nog 5 seconden slapen"
+        sleep 1
+        echo "Nog 4 seconden slapen"
+        sleep 1
+        echo "Nog 3 seconden slapen"
+        sleep 1
+        echo "Nog 2 seconden slapen"
+        sleep 1
+        echo "Nog 1 seconden slapen"
+        sleep 1
 }
 
 #for loop from 1/10 all the way up to 10/10
 for a in `seq 1 10`; do
 	echo "$a/10 to Exit."
-	sleep 1;
+	sleep 1
 done
 echo "exit example"
