f268ac2190f241110f0fa733358bfa012b7751b3
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 05:18:12 -0400

    3) Variables

--- linuxconfig-org/backup.sh
+++ linuxconfig-org/backup.sh
@@ -2,5 +2,7 @@
 #Author: Dhr. ing. B.A. Blaauwgeers
 # C: linuxconfig.org
 # T: 2) Simple Backup bash shell script
+# T: 3) Variables
 
-tar -czf myhome_directory.tar.gz /home/klaas/os3/linuxconfig-org
+OF=myhome_directory_$(date +%Y%m%d).tar.gz
+tar -czf $OF /home/klaas/os3/linuxconfig-org
--- linuxconfig-org/hello_world.sh
+++ linuxconfig-org/hello_world.sh
@@ -2,8 +2,9 @@
 #Author: Dhr. ing. B.A. Blaauwgeers
 # C: linuxconfig.org
 # T: 1)	Hallo World Bash Sell Script
+# T: 3) Variables
 
 # declare  STRING variable
-STRING="Hello World"
+STRING="HELLO WORLD!!!"
 #print variable on screen
 echo $STRING
