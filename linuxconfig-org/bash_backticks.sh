01d4600dcb9a14eb2b2f44a5bd51cc1ba712c8f7
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 07:52:19 -0400

    5) Executig shell commands with bash

---
+++ linuxconfig-org/bash_backticks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Dhr. ing. B.A. Blaauwgeers
+# Date: 29.04.2016
+# Version: v0.1
+# C: linuxconfig.org
+# T: 5) Executig shell commands with bash
+
+# use backtics " `` " to execute shell commands
+echo `uname -o` ' -> echo `uname -o` '
+# executing without tick on the back
+echo uname -o ' -> echo uname -o '
