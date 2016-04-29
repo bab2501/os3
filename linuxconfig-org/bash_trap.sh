2121f02fd1ab9869eb81b0358985cdd9a618f046
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 08:46:54 -0400

    7) bash trap

---
+++ linuxconfig-org/bash_trap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Dhr. ing. B.A. Blaauwgeers
+# Date: 29.04.2016
+# Version: v0.1
+# C: linuxconfig.org
+# T: 7) Bash trap command
+
+# bash trap command
+trap bashtrap INT
+
+# bash screen clear command
+clear;
+
+# bash prints message => executing trap subrutine
+
+bashtrap()
+{
+	echo "CTRL+C Detected !... executing bash trap!"
+}
+
+#for loop from 1/10 all the way up to 10/10
+for a in `seq 1 10`; do
+	echo "$a/10 to Exit."
+	sleep 1;
+done
+echo "exit example"
