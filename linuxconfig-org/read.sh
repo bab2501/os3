86a55f80263a5c21b31ec64952b4e8d8b2e2d78e
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 08:34:51 -0400

    colored read.sh

--- linuxconfig-org/read.sh
+++ linuxconfig-org/read.sh
@@ -5,16 +5,21 @@
 # C: linuxconfig.org
 # T: 6) Reading User Input
 
-echo -e "Hi Klaas, please type the word: \c "
+red=$(tput setaf 1)
+yellow=$(tput setaf 3)
+white=$(tput setaf 7)
+reset=$(tput sgr0)
+
+echo -e $yellow "Hi Klaas, please type the word: \c " $reset $white
 read word
-echo "The word that klaas entered is: $word"
-echo -e "Hi Klaas, can you please enter two words, I'd like that, ..?"
+echo $red "The word that klaas entered is: $word" $reset 
+echo -e $yellow "Hi Klaas, can you please enter two words, I'd like that, ..?" $reset $white
 read word1 word2
-echo "Here you are...: \"$word1\" \"$word2\""
-echo -e "How do you feel about Kees?"
+echo $red"Here you are...: \"$word1\" \"$word2\"" $reset
+echo -e $yellow "How do you feel about Kees?" $reset $white
 read
-echo "you siad $REPLY, I'm glad to hear that!"
-echo -e "what are your favorite colors?"
+echo $red "you siad $REPLY, I'm glad to hear that!" $reset
+echo -e $yellow "what are your favorite colors?" $reset $white
 # -a makes read command to readin an array
 read -a colors
-echo "My favorite colors are also ${colors[0]},  ${colors[1]} and ${colors[2]}"
+echo $red "My favorite colors are also ${colors[0]},  ${colors[1]} and ${colors[2]}" $reset
