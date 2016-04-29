d3f216ae8071bdbed29865016edc475c56b68c4e
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-04-29 07:41:58 -0400

    echo met een kleurtje

--- env-bashcolor-change.sh
+++ env-bashcolor-change.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #Auteur: Dhr. ing. B.A. Blaauwgeers
-#Script-Naam: Bash Color change v0.1
+#Script-Naam: Bash Color change v0.1.1
 #Functie: Dit script kleurt de BASH
+
+#Ref: http://ss64.com/bash/tput.html
+#Ref: http://ss64.com/bash/syntax-prompt.html
 
 magenta=$(tput setaf 5)
 blue=$(tput setaf 4)
 green=$(tput setaf 2)
 cyan=$(tput setaf 6)
+red=$(tput setaf 1)
 reset=$(tput sgr0)
-export PS1='[\[$green\]\u\[$reset\]@\[$blue\]\h\[$reset\]:\[$cyan\]\w\[$reset\]]\\$ '
+
+echo $green 'Dit script moet worden uitgevoerd via source scriptnaam.sh' $reset
+
+unset PS1
+export PS1='[\[$green\]\u\[$reset\]@\[$red\]\h\[$reset\]:\[$cyan\]\w\[$reset\]]\\$ '
 echo Whoopie, you are colorfull!!!
