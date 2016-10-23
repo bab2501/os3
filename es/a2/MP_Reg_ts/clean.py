77c3c3b6bf903deeb488a163eb1864a5de07f926
Alexander Blaauwgeers <alexander.blaauwgeers@hu.nl>
2016-10-23 17:19:08 -0400

    a2 ready

---
+++ es/a2/MP_Reg_ts/clean.py
@@ -0,0 +1,6 @@
+print "Starting the regex test"
+X = 5 
+if X == 5:
+  print "Working"
+else:
+  print "FAIL"
---
+++ es/a2/MP_Reg_ts/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# This should start the regex test
+print "Starting the regex test"
+X = 5 # Initialize X to be 5
+if X == 5:
+  print "Working"
+else:
+  print "FAIL"
+
