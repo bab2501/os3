cab7780f78f3baa779811534183e0f50fb9751e3
Alexander Blaauwgeers <ablaauwgeers@desktop-27.students.os3.nl>
2017-02-22 18:03:57 +0100

    take 3

--- lia/abtesting/abtest.sh
+++ lia/abtesting/abtest.sh
@@ -4,9 +4,9 @@ echo "The script starts soon."
 for i in {1..5}
 do
 	echo "Round $i"
-        echo "<h1>Round $i</hi>" >> result.html
-	ab -n 100000 -c 100 -w "http://145.100.106.20/" >> result.html
-	echo "<hr />" >> result.html
+        echo "=== Round $i ===" >> abresult.txt
+	ab -n 100000 -c 100 -w "http://145.100.106.20/" >> abresult.txt
 	sleep 1
 done
+vim abresult.txt -c "hardcopy > abresult.pdf | q"
 echo "The script is finished"
