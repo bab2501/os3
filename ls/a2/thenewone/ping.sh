fc05dbde4010c1e67e6892bc677e8f1313957811
Alexander Blaauwgeers <alexander@blaauwgeers.eu>
2017-11-07 19:00:24 +0100

    filename auto

--- ls/a2/thenewone/ping.sh
+++ ls/a2/thenewone/ping.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #Auteur: Dhr. ing. B.A. Blaauwgeers
-#Scriptname: LS Ping to Ireland v1
+#Scriptname: LS Ping to Ireland v2
 #Func: Measurement script for the LS lab 2.
+# v1: version used for cold
+# v2: added fast rename
 
 #onelineR
-sudo ping -D -i 0.000001 145.100.105.131 | while read pong; do echo "$pong"; done >> test03_cm.txt
+filename=$1
+sudo ping -D -i 0.000001 145.100.105.131 | while read pong; do echo "$pong"; done >> $filename
+
