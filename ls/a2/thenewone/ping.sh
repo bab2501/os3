a1b851cf34b8303f3df22e0725e4f36bca3ada80
Alexander Blaauwgeers <alexander@blaauwgeers.eu>
2017-11-09 15:28:19 +0100

    kibi

--- ls/a2/thenewone/ping.sh
+++ ls/a2/thenewone/ping.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #Auteur: Dhr. ing. B.A. Blaauwgeers
-#Scriptname: LS Ping to Ireland v2
+#Scriptname: LS Ping to Ireland v3
 #Func: Measurement script for the LS lab 2.
 # v1: version used for cold
 # v2: added fast rename
+# v3: To kibib
 
 #onelineR
 filename=$1
-sudo ping -D -i 0.000001 145.100.105.131 | while read pong; do echo "$pong"; done >> $filename
+sudo ping -D -s 1024 -i 0.000001 145.100.105.131 | while read pong; do echo "$pong"; done >> $filename
 
